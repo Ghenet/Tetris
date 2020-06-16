013e1d7507727ed53a9e837e30cc8bebd2fb544f
Ghenet Andemiachael <ghenetandemiachael@Ghenets-MacBook-Pro.local>
2020-06-16 13:23:07 -0700

    adds the L tetromino and its rotations

--- js/app.js
+++ js/app.js
@@ -4,4 +4,14 @@ document.addEventListener('DOMContentLoaded', () => {
 
     const width = 10;
     const height = 20;
+
+    //The tetriminos
+
+    const lTetromino = [
+        [1, width + 1, width * 2 + 1, 2],
+        [width, width + 1, width + 2, width * 2 + 2],
+        [1, width + 1, width * 2 + 1, width * 2],
+        [width, width * 2, width * 2 + 1, width * 2 + 2]
+
+    ]
 })
