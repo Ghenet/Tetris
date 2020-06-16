627b1890504730c6b0c17fb6ac2f700cc70679eb
Ghenet Andemiachael <ghenetandemiachael@Ghenets-MacBook-Pro.local>
2020-06-16 13:37:57 -0700

    adds the T tetromino and its rotations

--- js/app.js
+++ js/app.js
@@ -12,7 +12,6 @@ document.addEventListener('DOMContentLoaded', () => {
         [width, width + 1, width + 2, width * 2 + 2],
         [1, width + 1, width * 2 + 1, width * 2],
         [width, width * 2, width * 2 + 1, width * 2 + 2]
-
     ]
 
     const zTetromino = [
@@ -20,5 +19,12 @@ document.addEventListener('DOMContentLoaded', () => {
         [width + 1, width + 2, width * 2, width * 2 + 1],
         [0, width, width + 1, width * 2 + 1],
         [width + 1, width + 2, width * 2, width * 2 + 1]
+    ]
+
+    const tTetromino = [
+        [1, width, width + 1, width + 2],
+        [1, width + 1, width + 2, width * 2 + 1],
+        [width, width + 1, width + 2, width * 2 + 1],
+        [1, width, width + 1, width * 2 + 1]
     ]
 })
