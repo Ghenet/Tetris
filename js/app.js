4dc6115e51fc6ad2c16623b833212c3e8e562296
Ghenet Andemiachael <ghenetandemiachael@Ghenets-MacBook-Pro.local>
2020-08-26 10:19:08 -0700

    adds the moveDown function

--- js/app.js
+++ js/app.js
@@ -1,66 +1,77 @@
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener("DOMContentLoaded", () => {
+  const grid = document.querySelector(".grid");
+  let sqaures = Array.from(grid.querySelectorAll("div"));
+  const width = 10;
+  const height = 20;
+  let currentPosition = 4;
+  //The tetriminos
 
-    const grid = document.querySelector('.grid');
-    let sqaures = Array.from(grid.querySelectorAll('div'));
-    const width = 10;
-    const height = 20;
-    let currentPosition = 4;
-    //The tetriminos
+  const lTetromino = [
+    [1, width + 1, width * 2 + 1, 2],
+    [width, width + 1, width + 2, width * 2 + 2],
+    [1, width + 1, width * 2 + 1, width * 2],
+    [width, width * 2, width * 2 + 1, width * 2 + 2],
+  ];
 
-    const lTetromino = [
-        [1, width + 1, width * 2 + 1, 2],
-        [width, width + 1, width + 2, width * 2 + 2],
-        [1, width + 1, width * 2 + 1, width * 2],
-        [width, width * 2, width * 2 + 1, width * 2 + 2]
-    ]
+  const zTetromino = [
+    [0, width, width + 1, width * 2 + 1],
+    [width + 1, width + 2, width * 2, width * 2 + 1],
+    [0, width, width + 1, width * 2 + 1],
+    [width + 1, width + 2, width * 2, width * 2 + 1],
+  ];
 
-    const zTetromino = [
-        [0, width, width + 1, width * 2 + 1],
-        [width + 1, width + 2, width * 2, width * 2 + 1],
-        [0, width, width + 1, width * 2 + 1],
-        [width + 1, width + 2, width * 2, width * 2 + 1]
-    ]
+  const tTetromino = [
+    [1, width, width + 1, width + 2],
+    [1, width + 1, width + 2, width * 2 + 1],
+    [width, width + 1, width + 2, width * 2 + 1],
+    [1, width, width + 1, width * 2 + 1],
+  ];
 
-    const tTetromino = [
-        [1, width, width + 1, width + 2],
-        [1, width + 1, width + 2, width * 2 + 1],
-        [width, width + 1, width + 2, width * 2 + 1],
-        [1, width, width + 1, width * 2 + 1]
-    ]
+  const oTetromino = [
+    [0, 1, width, width + 1],
+    [0, 1, width, width + 1],
+    [0, 1, width, width + 1],
+    [0, 1, width, width + 1],
+  ];
+  const iTetromino = [
+    [1, width + 1, width * 2 + 1, width * 3 + 1],
+    [width, width + 1, width + 2, width + 3],
+    [1, width + 1, width * 2 + 1, width * 3 + 1],
+    [width, width + 1, width + 2, width + 3],
+  ];
 
-    const oTetromino = [
-        [0, 1, width, width + 1],
-        [0, 1, width, width + 1],
-        [0, 1, width, width + 1],
-        [0, 1, width, width + 1]
-    ]
-    const iTetromino = [
-        [1, width + 1, width * 2 + 1, width * 3 + 1],
-        [width, width + 1, width + 2, width + 3],
-        [1, width + 1, width * 2 + 1, width * 3 + 1],
-        [width, width + 1, width + 2, width + 3]
-    ]
+  const theTetrominoes = [
+    lTetromino,
+    zTetromino,
+    tTetromino,
+    oTetromino,
+    iTetromino,
+  ];
 
-    const theTetrominoes = [lTetromino, zTetromino, tTetromino, oTetromino, iTetromino]
+  //Randomly select Tetromino
+  let random = Math.floor(Math.random() * theTetrominoes.length);
+  let currentRotation = 0;
+  let current = theTetrominoes[random][currentRotation];
 
-    //Randomly select Tetromino
-    let random = Math.floor(Math.random() * theTetrominoes.length);
-    let currentRotation = 0;
-    let current = theTetrominoes[random][currentRotation];
+  //draw the shape
+  function draw() {
+    current.forEach((index) =>
+      sqaures[currentPosition + index].classList.add("block")
+    );
+  }
 
-    //draw the shape
-    function draw() {
-        current.forEach(index => (
-            sqaures[currentPosition + index].classList.add('block')
-        ))
-    }
+  //undraw the shape
+  function undraw() {
+    current.forEach((index) =>
+      sqaures[currentPosition + index].classList.remove("block")
+    );
+  }
 
-    //undraw the shape
-    function undraw() {
-        current.forEach(index => (
-            sqaures[currentPosition + index].classList.remove('block')
-        ))
-    }
-
-
-})
+  //move down shape
+  function moveDown() {
+    undraw();
+    currentPosition = currentPosition += width;
+    draw();
+    freeze();
+  }
+});
